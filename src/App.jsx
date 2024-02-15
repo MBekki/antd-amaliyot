e862d0ee4d7ce4e5530a55d64f4ce6c661523e93
Bekzod <bekzodmahammadov627@gmail.com>
2024-02-15 09:00:29 +0500

    app started

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+import Navigation from "./components/Navigation";
+
+const App = () => (
+  <div>
+    <Navigation />
+    <Sidebar />
+  </div>
+);
+
+export default App;
